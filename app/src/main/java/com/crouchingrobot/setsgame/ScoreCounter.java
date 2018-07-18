b30f81b791fe53df7d83a4e142923e0462cf888b
AlexM <alex.m.crouch@gmail.com>
2018-07-17 21:18:55 -0400

    Added a score counter

--- app/src/main/java/com/crouchingrobot/setsgame/ScoreCounter.java
+++ app/src/main/java/com/crouchingrobot/setsgame/ScoreCounter.java
@@ -6,20 +6,18 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 
-public class GameTimer implements GameObject {
+public class ScoreCounter implements GameObject {
     private Point center = null;
-    private long timeLeft;
-    private long startTime;
-    private long timeElapsed;
-    private long timerLength;
+    private int currentScore = 0;
     private Rect r = new Rect();
+    private int currentStreak = 0;
+    private int maxStreak = 0;
+    private int lastStreak = 0;
     private int textSize = 0;
 
     //Pass in timer length in seconds
-    public GameTimer(int startTimerLength, Point center, int textSize){
+    public ScoreCounter(Point center, int textSize){
         this.center = center;
-        timerLength = startTimerLength * 1000; //convert seconds to miliseconds
-        startTime = System.currentTimeMillis();
         this.textSize = textSize;
     }
 
@@ -28,11 +26,8 @@ public class GameTimer implements GameObject {
         Paint timerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         timerPaint.setColor(Color.WHITE);
         timerPaint.setTextSize(textSize);
-        String timeOut = "" + timeLeft/1000 + "";
-        //canvas.drawText(timeOut,(float)center.x,
-        //        (float)center.y- ((timerPaint.descent() + timerPaint.ascent()) / 2),
-        //        timerPaint);
-        drawCenter(canvas, timerPaint,timeOut);
+        String scoreOut = "" + currentScore + "";
+        drawCenter(canvas, timerPaint,scoreOut);
     }
 
     private void drawCenter(Canvas canvas, Paint paint, String text) {
@@ -48,15 +43,34 @@ public class GameTimer implements GameObject {
 
     @Override
     public void update() {
-        timeElapsed = System.currentTimeMillis() - startTime;
-        timeLeft = timerLength - timeElapsed;
+
     }
 
-    public void addTime(int timeToExtend){
-        timerLength += (timeToExtend * 1000);   //convert seconds to miliseconds
+    public void addPoints(int scoreToAdd){
+        currentScore += (scoreToAdd * currentStreak);
     }
 
-    public boolean up (){
-        return (timeLeft <= 0);
+    public int getScore(){
+        return currentScore;
+    }
+
+    public void streakInc(){
+        currentStreak++;
+    }
+
+    public void streakEnd(){
+        if(maxStreak<currentStreak){
+            maxStreak = currentStreak;
+        }
+        lastStreak = currentStreak;
+        currentStreak = 0;
+    }
+
+    public void setToMax(){
+        currentStreak = maxStreak;
+    }
+
+    public void setToLast(){
+        currentStreak = lastStreak;
     }
 }
