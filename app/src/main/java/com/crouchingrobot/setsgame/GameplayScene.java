3167da85258afe635e0595c8c78aca79ff7da8f6
AlexM <alex.m.crouch@gmail.com>
2018-07-21 19:32:16 -0400

    Added scene management.

---
+++ app/src/main/java/com/crouchingrobot/setsgame/GameplayScene.java
@@ -0,0 +1,31 @@
+package com.crouchingrobot.setsgame;
+
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+
+public class GameplayScene implements Scene {
+    private SceneManager manager;
+
+    public GameplayScene(){
+    }
+
+    @Override
+    public void recieveTouch(MotionEvent event) {
+
+    }
+
+    @Override
+    public void terminate() {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
