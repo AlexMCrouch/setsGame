2c19adde2cca46270aad5f82fa2ef3c63dea6dc3
AlexMCrouch <alex.m.crouch@gmail.com>
2019-03-03 11:54:19 -0500

    Removed game activity and folded back to just main activity

--- app/src/main/java/com/crouchingrobot/setsgame/MainActivity.java
+++ app/src/main/java/com/crouchingrobot/setsgame/MainActivity.java
@@ -19,67 +19,202 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends Activity {
+
+    private SceneManager manager = new SceneManager();
+    private long backHitTime = 0;
     public Intent intent;
+    private RelativeLayout layout = null;
+    View gameView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        intent = new Intent(this, GameActivity.class);
-        //EditText editText = (EditText) findViewById(R.id.FullscreenActivity);
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-        finish();
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //This is my Ad ID so use this for publishing
+        //MobileAds.initialize(this, "ca-app-pub-3501821828458096~4941816590");
+
+        //Create the banner ad
+        final AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.SMART_BANNER);
+        //This is the test Ad ID so use this for testing
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+        //This is the real Ad ID so use this for publishing
+        //adView.setAdUnitId("ca-app-pub-3501821828458096/3702980440");
+
+//        //Create the interstatial ad
+//        mInterstitialAd = new InterstitialAd(this);
+//        //Test ad ID
+//        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+//        //Real ad ID
+//        //mInterstitialAd.setAdUnitId("ca-app-pub-3501821828458096/4826212622");
+
+
+        // Add the AdView to the view hierarchy. The view will have no size
+        // until the ad is loaded.
+        layout = new RelativeLayout(this);
+        layout.setLayoutParams(new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT,    WindowManager.LayoutParams.MATCH_PARENT));
+        // Create an ad request.
+        // get test ads on a physical device.
+        final AdRequest adRequest = new AdRequest.Builder().build();
+
+
+
+
+
+        //define ad view parameters
+        final RelativeLayout.LayoutParams adParams =
+                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+        adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        adParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+
+
+        manager = new SceneManager();
+        //Create game view
+        View gameView = new GamePanel(this,manager);
+        manager.createScenes();
+        ((JustLostScene)manager.scenes.get(2)).gHscore.readHighScore(this);
+
+
+
+        setContentView(R.layout.activity_main);
+        layout.addView(gameView);
+
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(layout);
+
+        final Handler handler = new Handler();
+        Runnable r = new Runnable() {
+            private int lastScene = 0;
+
+            boolean interAdLoading = false;
+            public void run() {
+                if(SceneManager.ACTIVE_SCENE == 1){
+                    if(lastScene != 1){
+                        adView.setVisibility(View.INVISIBLE);
+                        // Start loading the ad in the background.
+                        adView.loadAd(adRequest);
+                        layout.removeView(adView);
+                    }
+                }else{
+                    if(lastScene == 1){
+
+
+//                        if (mInterstitialAd.isLoaded()) {
+//                            mInterstitialAd.show();
+//                            interAdLoading = false;
+//                            System.out.println("Trying to show");
+//                        }else{
+//                            if(!interAdLoading){
+//                                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+//                                interAdLoading = true;
+//                                System.out.println("starting loading");
+
+                        //display banner ad since interstatal is not ready
+                        adView.setVisibility(View.VISIBLE);
+                        layout.addView(adView, adParams);
+
+
+//                            }
+                        //}
+                    }
+                }
+                lastScene = SceneManager.ACTIVE_SCENE;
+            }
+        };
+
+        handler.postDelayed(r, 100);
+
+
 
     }
 
+    void writeHighScore(){
+
+        if(manager.scenes.get(2)!= null){
+            ((JustLostScene)manager.scenes.get(2)).gHscore.writeHighScore(this);
+        }
+    }
 
     protected void onResume() {
         // TODO Auto-generated method stub
-        System.out.println("Resume Main");
+        System.out.println("Resume Game");
         super.onResume();
-        startGameActivity();
+        manager = new SceneManager();
+        //Create game view
+        gameView = new GamePanel(this,manager);
+        manager.createScenes();
+        ((JustLostScene)manager.scenes.get(2)).gHscore.readHighScore(this);
+
+
+
+        setContentView(R.layout.activity_main);
+        layout.requestLayout();
+        layout.addView(gameView);
+        setContentView(layout);
     }
 
     @Override
     protected void onPause() {
         // TODO Auto-generated method stub
-        System.out.println("onPause Main");
-        //System.exit(0);
+
+        writeHighScore();
+        System.out.println("onPause Game");
         super.onPause();
+        ((GamePanel)gameView).destroyThread();
+        manager = null;
+        gameView = null;
     }
 
     @Override
     protected void onDestroy() {
         // TODO Auto-generated method stub
-        System.out.println("onDestroy Main");
+        System.out.println("onDestroy Game");
         super.onDestroy();
     }
 
     @Override
     protected void onStart() {
 
-        System.out.println("onStart Main");
+        System.out.println("onStart Game");
         super.onStart();
     }
 
     @Override
     protected void onStop() {
 
-        System.out.println("onStop Main");
+        System.out.println("onStop Game");
         super.onStop();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        System.out.println("outState Main");
+        System.out.println("outState Game");
+        writeHighScore();
         super.onSaveInstanceState(outState);
     }
+    @Override
+    public void onBackPressed() {
+        if(backHitTime + 1000 > System.currentTimeMillis()){
+            moveTaskToBack(true);
+        }else{
+            backHitTime = System.currentTimeMillis();
+        }
 
-    private void startGameActivity(){
-        intent = new Intent(this, GameActivity.class);
+    }
+
+    private void backToMain(){
+        intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
-
 }
 
